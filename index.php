3be81f29063daee805c075c10e13a2bb2c71debc
Meirlianah <79677008+Meirlianah@users.noreply.github.com>
2021-03-07 08:50:55 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Objek Wisata Pantai Lebih</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+<div class="container" style="background-color: grey;">
+     <div class="page-header">
+    <h1>Objek Wisata Pantai Lebih</h1>
+</div>
+
+<div class="container">
+    <ul class="nav nav-tabs">
+    <li class="active"><a data-toggle="tab" href="#home">Home</a></li>
+    <li><a data-toggle="tab" href="#menu1">Aktivitas</a></li>
+    <li><a data-toggle="tab" href="#menu2">About</a></li>
+    <div class="container" style="width: 700px;">  
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+        <img src="media/Pantai-Dreamland.jpg" alt="Los Angeles" style="width:100%;">
+      </div>
+
+      <div class="item">
+        <img src="media/Objek-wisata-pantai-Lebih.jpg" alt="Chicago" style="width:100%;">
+      </div>
+    
+      <div class="item">
+        <img src="media/ombak-7-meter-terjang-pantai-lebih-800-2019-06-20-125903_0.jpg" alt="New york" style="width:100%;">
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+    </ul>
+</div>
+
+<div class="container" style="background-color: burlywood;" float: left; width: 630px; margin: 15px;">
+    <h1><small>Keindahan Pantai Lebih</small></h1>
+    <h3><small>Menikmati kawasan pesisir pantai Bali, anda bisa coba ketenangan di pantai Lebih, lokasinya di sebelah Selatan ibu kota Gianyar. Pasir pantainya berwarna hitam berkilau, terlihat beda dengan pesisir pantai bagian Selatan seperti Kuta, Dreamland ataupun Tanjung Benoa yang didominasi oleh panati berpasir putih. Walaupun pantai Lebih ini berpasir hitam, namun demikian pesonanya juga tidak bisa dipandang sebelah mata saja, hamparan pantainya membentang dari Timur ke Barat sekitar 1.8 km, air lautnya berwarna biru jernih di hadapan pantai di ujung Selatan terlihat dataran pulau Nusa Penida.</small></h3>
+    <img src="media/PantaiLebih.png" class="img-thumbnail" alt="Cinque Terre" width="608" height="472"> 
+    <h1><small>Lokasi Pantai Lebih</small></h1>
+    <h3><small>Jika anda tertarik untuk mengunjungi pantai yang memiliki pasir hitam legam ini maka anda bisa langsung datang ke desa lebih, tepatnya di kecamatan dan Kabupaten Gianyar Bali. Jika anda sedang mengunjungi beberapa tempat wisata di Gianyar seperti rekreasi Ayung rafting ataupun Goa gajah maka anda bisa mampir ke pantai ini. Selain itu, rute untuk menuju ke pantai ini sendiri cukup mudah dan bisa diakses melalui jalan raya By Pass Ida Bagus Mantra. Dengan rute yang berada di jalan raya inilah maka akan memudahkan Anda yang tidak membawa kendaraan pribadi. Sampai di Pantai Pandan langsung akan terpesona melihat keindahan pantai ini.</small></h3>
+    <h1><small>Kuliner Pantai Lebih</small></h1>
+    <h3><small>Ketika anda sudah sampai Ke kawasan pantai ini maka jangan sampai melewatkan untuk mencoba sajian makanan laut. Nelayan yang ada di kawasan ini sendiri biasanya memiliki tangkapan yang didominasi oleh ikan tuna, ikan marlin dan tenggiri. Di tempat ini Anda juga bisa mencoba sate luluh yang dijual di warung tidak jauh dari kawasan pantai.</small></h3>
+    <img src="media/sate.png" class="img-thumbnail" alt="Cinque Terre" width="608" height="472">
+</div>
+
+<div class="col-lg-4  " style="background-color: coral;"  float: right; width: 450px; margin: 15px;"> 
+    <div  class="widget  ">
+        <h2>Terpopular</h2>
+        <ul  class="menusidebar  ">
+            <li><a  href="  ">Kondisi Terkini Nissa Sabyan</a></li>
+            <li><a  href="  ">Banjir di Jakarta Berstatus Siaga</a></li>
+            <li><a  href="  ">Rencana Pelantikan Gibran</a></li>
+            <li><a  href="  ">Penggunaan Tali Masker Dinilai Berbahaya</a></li>
+        </ul>
+<div class="container ">    
+    <div class="content">
+        <div class="col-lg-8 bg-info" style="background-color: skyblue ; width: 390px">
+          <h2>Leave a Reply</h2>   
+          <hr>
+          <p>Alamat email anda tidak akan dipublikasikan</p>
+          <form class=" form-horizontal " action=" ">
+            <div class="form-group ">
+              <label class="control-label col-sm-3 ">Nama:</label>
+              <div class="col-sm-9 ">
+                  <input type="text" class="form- control " required placeholder="Masukkan Nama " name="nama ">
+              </div>
+            </div>
+            <div class="form-group ">
+              <label class="control-label col-sm-3 ">Email:</label>
+              <div class="col-sm-9 ">
+                <input type="email" class="form- control " placeholder="Email " name="email ">
+              </div>
+            </div>
+            <div class="form-group ">
+              <label class="control-label col-sm-3 ">Password:</label>
+              <div class="col-sm-9 ">
+                <input type="password" class="form- control psw " placeholder="Enter password ">
+              </div>
+            </div>
+            <div class="form-group ">
+              <label class="control-label col-sm-3 ">Re-Password:</label>
+              <div class="col-sm-9 ">
+                <input type="password" class="form- control repsw " placeholder="Enter password ">
+                <small class="errpsw "></small>
+              </div>
+            </div>
+            <div class="form-group ">
+              <label class="control-label col-sm-3 ">Comment:</label>
+              <div class="col-sm-9 ">
+                <textarea type="text" class="form- control " rows="3" required placeholder="Masukkan Komentar " name="comment "></textarea>
+              </div>
+            </div>
+            <div class="form-group ">
+              <div class="col-sm-offset-2 col-sm-9 ">
+                <div class="checkbox ">
+                  <label><input type="checkbox" name="remember "> Remember me</label>
+                </div>
+              </div>
+            </div>
+            <div class="form-group ">
+              <div class="col-sm-offset-3 col-sm-9 ">
+                <button type="submit " class="btn btn-default ">Submit</button>
+              </div>
+            </div>
+          </form>
+        </div>
+    </div>
+  </div>
+  <div id="myModal " class="modal fade " role="dialog ">
+    <div class="modal-dialog ">
+      <!-- Modal content-->
+      <div class="modal-content ">
+        <div class="modal-header ">
+          <button type="button " class="close " data- dismiss="modal ">&times;</button>
+          <h4 class="modal-title ">Berhasil</h4>
+        </div>
+        <div class="modal-body ">
+          <p></p>
+        </div>
+        <div class="modal-footer ">
+          <button type="button " class="btn btn-default " data- dismiss="modal ">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <hr>
+  <p>copyright 2021</p>
+</div>
+
+</body>
+
+<script>
+  $(document).ready(function() {
+    $(".form-horizontal ").submit(function() {
+      var ps1 = $('.psw').val();
+      var ps2 = $('.repsw').val(); 
+      var valid = 0;
+      $('.err').html(''); 
+      if (ps1 == ps2) {
+        valid = 1;
+      } else {
+        $('.errpsw').html('Passsword Tidak Sama');
+      }
+      if (valid == 0) { 
+        event.preventDefault();
+      } else {
+        $('p').html("Kommentar Atas Nama " + $('input[name="nama "]').val() + " Berhasil Ditambahkan. Terima Kasih dan Dukung Terus Chanel Ini ");
+        $('.modal').modal('show'); 
+        event.preventDefault();
+      }
+    });
+  });
+</script>
+
+
+;"
